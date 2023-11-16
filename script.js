aa781265c926195d563d798f799205f34679425d
Daniel Harding <danielharding2008@gmail.com>
2023-11-16 16:23:02 +0000

    added html styling and js save button features

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var dateEl = $("#currentDay");
-var today = dayjs();
-var currentTIme = $("<p>");
-currentTIme.text(today);
-currentTIme.appendTo(dateEl);
 
-console.log(dayjs());
+var currentTIme = $("<p>");
+
+var time = setInterval(Timer, 1000);
+function Timer() {
+  var today = dayjs();
+  currentTIme.text(today);
+  currentTIme.appendTo(dateEl);
+}
+
+$(".savebtn").on("click", function () {
+  var targetVal = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, targetVal);
+});
